7dda2714833b906b55ee9451ea5f35451b1e9787
Jst_Kakkau <kauavdsb.jobs@gmail.com>
2025-07-28 01:37:17 -0300

    Socio guardiao (#1)
    * feat: implementa integracao do mercadopago para pagamentos
    
    * chore: update .gitignore para incluir __pycache__ e *.pyc e remove bug do .env
    
    * feat: cria html base, configura pagamentos no mercado pago e add novas rotas.
    
    * refactor: atualiza vercel.json para direcionar rotas para main.py
    
    * feat: atualiza rota para socio-guardiao route e template para incluir links de pagamento; refatora links de navegacao em header e footer

--- guardioesverdade/routes.py
+++ guardioesverdade/routes.py
@@ -1,8 +1,73 @@
 from flask import render_template
 
 from guardioesverdade import app
+from guardioesverdade.mercadopago.api_mp import gera_link_pagamento
 
 @app.route("/")
 def homepage():
 
     return render_template("index.html")
+
+
+@app.route("/doacoes")
+def donate():
+    d_1 = gera_link_pagamento(1)
+    d_15 = gera_link_pagamento(15)
+    d_30 = gera_link_pagamento(30)
+    d_50 = gera_link_pagamento(50)
+    # d_livre = gerar_link_pagamento(donate_livre)
+    
+    return render_template(
+        "mercado-pago/doacao.html", d1 = d_1, d15 = d_15, d30 =d_30, d50 = d_50,
+        # dlivre = d_livre
+    )
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template("pages/sobre.html")
+
+
+@app.route("/album")
+def album():
+    return render_template("pages/album.html")
+
+
+@app.route("/classes")
+def classes():
+    return render_template("pages/classes.html")
+
+    
+@app.route("/contato")
+def contato():
+    return render_template("pages/contato.html")
+
+@app.route("/dracmas")
+def dracmas():
+    return render_template("pages/dracmas.html")
+
+@app.route("/eventos")
+def eventos():
+    return render_template("pages/eventos.html")
+
+@app.route("/socio-guardiao")
+def socioguardiao():
+    d_15 = gera_link_pagamento(15)
+    d_30 = gera_link_pagamento(30)
+    d_50 = gera_link_pagamento(50)
+    d_100 = gera_link_pagamento(100)
+    # d_livre = gerar_link_pagamento(donate_livre)
+    
+    return render_template(
+        "pages/socio-guardiao.html", d15 = d_15, d30 =d_30, d50 = d_50, d100 = d_100,
+        # dlivre = d_livre
+    )
+
+@app.route("/arearestrita")
+def arearestrita():
+    return render_template("pages/arearestrita.html")
+
+@app.route("/unidades")
+def unidades():
+    return render_template("pages/unidades.html")
+
