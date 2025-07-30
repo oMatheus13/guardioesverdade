d863d988adf440c083c4450d04dbb9b5f7be8078
KakisCoding <kauavdsb.jobs@gmail.com>
2025-07-30 20:05:23 -0300

    (feat): Inicia configuracoes para Login e Banco de dados - Supabase - em __init__.py

--- guardioesverdade/__init__.py
+++ guardioesverdade/__init__.py
@@ -1,6 +1,14 @@
 import os
 
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+from supabase import create_client
+
 from dotenv import load_dotenv
 
 load_dotenv(".env")
@@ -8,6 +16,21 @@ load_dotenv(".env")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager(app)
+login_manager.login_view = (
+    "homepage"  # Redireciona usuário para a homepage se não estiver logado
+)
 
 
 from guardioesverdade.routes import homepage
--- guardioesverdade/routes.py
+++ guardioesverdade/routes.py
@@ -3,25 +3,29 @@ from flask import render_template
 from guardioesverdade import app
 from guardioesverdade.mercadopago.api_mp import gera_link_pagamento
 
+
 @app.route("/")
 def homepage():
 
     return render_template("index.html")
 
 
-@app.route("/doacoes")
-def donate():
-    d_1 = gera_link_pagamento(1)
+@app.route("/login")
+def login():
+    return render_template("login/login.html")
+
+@app.route("/socio-guardiao")
+def socioguardiao():
     d_15 = gera_link_pagamento(15)
     d_30 = gera_link_pagamento(30)
     d_50 = gera_link_pagamento(50)
+    d_100 = gera_link_pagamento(100)
     # d_livre = gerar_link_pagamento(donate_livre)
     
     return render_template(
-        "mercado-pago/doacao.html", d1 = d_1, d15 = d_15, d30 =d_30, d50 = d_50,
+        "pages/socio-guardiao.html", d15 = d_15, d30 =d_30, d50 = d_50, d100 = d_100,
         # dlivre = d_livre
     )
-
 
 @app.route("/sobre")
 def sobre():
@@ -50,19 +54,6 @@ def dracmas():
 def eventos():
     return render_template("pages/eventos.html")
 
-@app.route("/socio-guardiao")
-def socioguardiao():
-    d_15 = gera_link_pagamento(15)
-    d_30 = gera_link_pagamento(30)
-    d_50 = gera_link_pagamento(50)
-    d_100 = gera_link_pagamento(100)
-    # d_livre = gerar_link_pagamento(donate_livre)
-    
-    return render_template(
-        "pages/socio-guardiao.html", d15 = d_15, d30 =d_30, d50 = d_50, d100 = d_100,
-        # dlivre = d_livre
-    )
-
 @app.route("/arearestrita")
 def arearestrita():
     return render_template("pages/arearestrita.html")
@@ -71,3 +62,16 @@ def arearestrita():
 def unidades():
     return render_template("pages/unidades.html")
 
+
+# @app.route("/doacoes")
+# def donate():
+#     d_1 = gera_link_pagamento(1)
+#     d_15 = gera_link_pagamento(15)
+#     d_30 = gera_link_pagamento(30)
+#     d_50 = gera_link_pagamento(50)
+#     # d_livre = gerar_link_pagamento(donate_livre)
+    
+#     return render_template(
+#         "mercado-pago/doacao.html", d1 = d_1, d15 = d_15, d30 =d_30, d50 = d_50,
+#         # dlivre = d_livre
+#     )
